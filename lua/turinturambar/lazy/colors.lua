88d7302d8d7e961836263e2acb23f7683593da30
andrew <andrew@athompson.us>
2024-04-19 10:42:39 -0500

    colors and telescope bindings

--- lua/turinturambar/lazy/colors.lua
+++ lua/turinturambar/lazy/colors.lua
@@ -3,6 +3,8 @@ function ColorStuff(color)
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "@attribute", { fg = "gold" })
+	vim.api.nvim_set_hl(0, "@lsp.type.attribute", { fg = "gold" })
 
 end
 
