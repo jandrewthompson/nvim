a299fe4021ee3ebea35c34d1c08efe509c92b1e6
J. Andrew Thompson <jandrewthompson@gmail.com>
2024-05-13 20:46:40 -0500

    LSP completion updates

--- lua/turinturambar/lazy/lsp.lua
+++ lua/turinturambar/lazy/lsp.lua
@@ -8,7 +8,15 @@ return {
         {'neovim/nvim-lspconfig'},
         {'hrsh7th/cmp-nvim-lsp'},
         {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-nvim-lua'},
+        {'hrsh7th/cmp-nvim-lsp-signature-help'},
+        {'hrsh7th/cmp-vsnip'},
+        {'hrsh7th/cmp-path'},
+        {'hrsh7th/cmp-calc'},
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/vim-vsnip'},
         {'L3MON4D3/LuaSnip'},
+        {'mfussenegger/nvim-dap'},
     },
     config = function()
         ---
@@ -69,7 +77,37 @@ return {
                     require('luasnip').lsp_expand(args.body)
                 end,
             },
+            sources = {
+                { name = 'path' },                              -- file paths
+                { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
+                { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
+                { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
+                { name = 'buffer', keyword_length = 2 },        -- source current buffer
+                { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+                { name = 'calc'},                               -- source for math calculation
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            formatting = {
+                fields = {'menu', 'abbr', 'kind'},
+                format = function(entry, item)
+                    local menu_icon ={
+                        nvim_lsp = 'λ',
+                        vsnip = '⋗',
+                        buffer = 'Ω',
+                        path = '🖫',
+                        calc = '🖩',
+                    }
+                    item.menu = menu_icon[entry.source.name]
+                    return item
+                end,
+            },
         })
+
+
+
 
     end
 }
