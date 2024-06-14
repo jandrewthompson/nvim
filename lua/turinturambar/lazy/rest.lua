9666f3888041e7076138f779324365fcdbd4a413
J. Andrew Thompson <jandrewthompson@gmail.com>
2024-06-14 09:23:08 -0500

    lazy rest config

--- lua/turinturambar/lazy/rest.lua
+++ lua/turinturambar/lazy/rest.lua
@@ -10,11 +10,24 @@ return {
         dependencies = { "luarocks.nvim" },
         config = function()
             require("rest-nvim").setup({
+                logs = {
+                    level = "trace",
+                    save = true,
+                },
                 result = {
                     split = {
                         horizontal = false,
                         in_place = true,
                         stay_in_current_window_after_split = true,
+                    },
+                    behavior = {
+                        decode_url = true,
+                        show_info = {
+                            url = true,
+                            headers = true,
+                            http_info = true,
+                            curl_command = true,
+                        },
                     },
                 },
                 keybinds = {
