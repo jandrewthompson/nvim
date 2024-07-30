757be2864e6ac4dad8e93b96caf7dad1f5025674
J. Andrew Thompson <jandrewthompson@gmail.com>
2024-07-30 16:23:05 -0500

    update clj

--- lua/turinturambar/lazy/clj.lua
+++ lua/turinturambar/lazy/clj.lua
@@ -3,6 +3,11 @@ return {
     dependencies = {
         "clojure-vim/vim-jack-in",
         "tpope/vim-dispatch",
-        "radenling/vim-dispatch-neovim"
-    }
+        "radenling/vim-dispatch-neovim",
+        "julienvincent/nvim-paredit",
+    },
+    config = function()
+        require('nvim-paredit').setup()
+    end
+
 }
